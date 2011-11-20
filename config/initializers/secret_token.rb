4ae1e9e34c598019c1e6a7f14b74720832e81007
StrongTSQ <sxt286@case.edu>
2011-11-20 17:07:55 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PedigreeVisualization::Application.config.secret_token = '8202eae867ca6f45c58837555ac95b69f9040e363d765d98ddb94c2537301c46045496a5e2748e2c087426565c1a311d2dcebff7c6ac76d15f6894d8f0e0eb6c'
